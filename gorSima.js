f0522a8f0144a9f0071eb70193c9ba418ee91c8e
fznhakiim <zanziah@gmail.com>
2024-06-23 22:27:23 +0700

    first commit

---
+++ gorSima.js
@@ -0,0 +1,10 @@
+// Navbar
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+}
+// Navbar
